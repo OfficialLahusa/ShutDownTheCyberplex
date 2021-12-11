eb9abbad6f831400a77cafa5cb9d48a0eba28cc4
Lasse Huber-Saffer <lasse-huber-saffer@web.de>
2021-12-11 17:32:52 +0100

    Fixed State Transition

--- TitleScene.java
+++ TitleScene.java
@@ -9,11 +9,8 @@ public class TitleScene extends Scene
 {
     private Camera _camera;
     private BarAudioVisualizer _audioVisualizer;
-    private Renderer _renderer;
-    private TextRenderer _textRenderer;
     private StaticGameObject _title;
     private StaticGameObject _mauern;
-    private boolean started = false;
     
     
     public TitleScene(GameState state)
@@ -26,35 +23,23 @@ public class TitleScene extends Scene
         
         _state.soundRegistry.loadSource("music2", "./res/sounds/midnight_drive.mp3");
         
-        _renderer = new Renderer();
-        _textRenderer = new TextRenderer(_renderer);
+        Sound backgroundMusic = _state.soundRegistry.playSound("music2", 0.35, true);
+    
+        //backgroundMusic.setAudioSpectrumNumBands(256+128);
+        backgroundMusic.setAudioSpectrumNumBands(96);
+        backgroundMusic.setAudioSpectrumInterval(0.075);
+        //backgroundMusic.setAudioSpectrumThreshold(-200);
+        backgroundMusic.setAudioSpectrumThreshold(-80);
+        
+        _audioVisualizer = new BarAudioVisualizer(backgroundMusic);
+        backgroundMusic.setAudioSpectrumListener(_audioVisualizer);
+        
         _title = new StaticGameObject(_state.objLoader.loadFromFile("./res/models/title.obj"), "cyan", new Vector3 (0.0, 5, -14));
         _mauern = new StaticGameObject(_state.objLoader.loadFromFile("./res/models/mauern.obj"), "cyan", new Vector3 (-1.48, 0.34, -2.5));
     }
     
     public void handleInput(double deltaTime, double runTime)
     {
-        if(!started && _state.inputHandler.isKeyPressed(KeyCode.KEY_SPACE))
-        {
-            started = true;
-            
-            Sound backgroundMusic = _state.soundRegistry.playSound("music2", 0.35, true);
-        
-            //backgroundMusic.setAudioSpectrumNumBands(256+128);
-            backgroundMusic.setAudioSpectrumNumBands(96);
-            backgroundMusic.setAudioSpectrumInterval(0.075);
-            //backgroundMusic.setAudioSpectrumThreshold(-200);
-            backgroundMusic.setAudioSpectrumThreshold(-80);
-            
-            _audioVisualizer = new BarAudioVisualizer(backgroundMusic);
-            backgroundMusic.setAudioSpectrumListener(_audioVisualizer);
-        }
-        
-        if(_state.inputHandler.isKeyPressed(KeyCode.KEY_PLUS))
-        {
-            _state.soundRegistry.removeAllSounds();
-            _state.scene = new GameScene(_state);
-        }
     }
     
     public void update(double deltaTime, double runTime)
@@ -66,8 +51,8 @@ public class TitleScene extends Scene
     {
         _state.renderer.clear(10, 10, 10);
         if(_audioVisualizer != null) _audioVisualizer.draw(_state.renderer, _camera);
-        _title.draw(_renderer, _camera);
-        _mauern.draw(_renderer, _camera);
+        _title.draw(_state.renderer, _camera);
+        _mauern.draw(_state.renderer, _camera);
         
         //draw Start Button
         drawButton("Start");
@@ -84,30 +69,31 @@ public class TitleScene extends Scene
         //draw button text
         if(text == "Start")
         {
-            _textRenderer.write(new Vector2(220.9,372), 6, text, "weiss");
+            _state.textRenderer.write(new Vector2(220.9,372), 6, text, "weiss");
         }
         else
         {
             p = 42;
-            _textRenderer.write(new Vector2(250-37.65,414), 6,text, "weiss");
+            _state.textRenderer.write(new Vector2(250-37.65,414), 6,text, "weiss");
         }
         
         //draw button box
         if(mousePos.getX() > 196 && mousePos.getX() < 304 && mousePos.getY() > 364+p && mousePos.getY() < 396+p)
         {
-            _renderer.drawLine(new Vector2(231, 364+p), new Vector2(269, 364+p), "magenta");
-            _renderer.drawLine(new Vector2(269, 364+p), new Vector2(304, 370+p), "magenta");
-            _renderer.drawLine(new Vector2(304, 370+p), new Vector2(304, 390+p), "magenta");
-            _renderer.drawLine(new Vector2(304, 390+p), new Vector2(269, 396+p), "magenta");
-            _renderer.drawLine(new Vector2(269, 396+p), new Vector2(231, 396+p), "magenta");
-            _renderer.drawLine(new Vector2(231, 396+p), new Vector2(196, 390+p), "magenta");
-            _renderer.drawLine(new Vector2(196, 390+p), new Vector2(196, 370+p), "magenta");
-            _renderer.drawLine(new Vector2(196, 370+p), new Vector2(231, 364+p), "magenta");
+            _state.renderer.drawLine(new Vector2(231, 364+p), new Vector2(269, 364+p), "magenta");
+            _state.renderer.drawLine(new Vector2(269, 364+p), new Vector2(304, 370+p), "magenta");
+            _state.renderer.drawLine(new Vector2(304, 370+p), new Vector2(304, 390+p), "magenta");
+            _state.renderer.drawLine(new Vector2(304, 390+p), new Vector2(269, 396+p), "magenta");
+            _state.renderer.drawLine(new Vector2(269, 396+p), new Vector2(231, 396+p), "magenta");
+            _state.renderer.drawLine(new Vector2(231, 396+p), new Vector2(196, 390+p), "magenta");
+            _state.renderer.drawLine(new Vector2(196, 390+p), new Vector2(196, 370+p), "magenta");
+            _state.renderer.drawLine(new Vector2(196, 370+p), new Vector2(231, 364+p), "magenta");
             
             // NOTE(sven): Das könnte man noch besser machen, indem wir fragen ob die Taste auch wieder auf dem Button losgelassen wurde, 
             // damit man wirklich einen ganzen Klick auf dem Button bleiben muss.
             if (_state.inputHandler.isKeyPressed(KeyCode.MOUSE_BUTTON_LEFT))
             {
+                _state.soundRegistry.removeAllSounds();
                 _state.scene = new GameScene(_state);
             }
         }
