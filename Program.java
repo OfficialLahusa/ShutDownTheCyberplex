5473ec5640737dd9848ed58d673569c47b2bf2a7
Lasse Huber-Saffer <lasse-huber-saffer@web.de>
2021-12-16 04:59:47 +0100

    Added Program, Added Run Script

---
+++ Program.java
@@ -0,0 +1,15 @@
+
+/**
+ * Höchstgeordnetes Hauptprogramm
+ * 
+ * @author Lasse Huber-Saffer
+ * @version 16.12.2021
+ */
+public class Program
+{
+    public static void main(String[] args)
+    {
+        Game game = new Game();
+        game.start();
+    }
+}
---
+++ run.ps1
@@ -0,0 +1,1 @@
+java --module-path "C:\Program Files\BlueJ\lib\javafx\lib" --add-modules javafx.swing,javafx.media -jar ShutDownTheCyberplex.jar
