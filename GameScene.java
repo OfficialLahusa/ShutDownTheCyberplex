2e1e508f5d7ab06089529d0989f3ed73eb898fb6
Lasse Huber-Saffer <lasse-huber-saffer@web.de>
2022-01-03 20:01:27 +0100

    Added PlayerViewModel Object, Implemented Muzzle Flash Scaling, Readded .ctxt Files

--- GameScene.java
+++ GameScene.java
@@ -8,13 +8,9 @@ import java.util.*;
  */
 public class GameScene extends Scene
 {
-    private MapHandler _mapHandler;
-    private DynamicViewModelGameObject _test, _muzzleFlash, _pistolMain, _primaryMain, _sniperMain, _pistolDetails, _primaryDetails, _sniperDetails, _pistolHandsIdle, _pistolHandsShot, _primaryHandsIdle, _primaryHandsShot;
-    
-    private Player _player;    
-    private double _timeSinceLastShot;
-
-    private static final double PISTOL_SHOT_COOLDOWN = 0.3;
+    private MapHandler _mapHandler;    
+    private Player _player;
+    private PlayerViewModel _viewModel;
     
     public GameScene(GameState state)
     {
@@ -28,18 +24,7 @@ public class GameScene extends Scene
         
         _mapHandler = new MapHandler(tileMeshes, entityMeshes, particleMeshes, _state.soundEngine);
         
-        // Viewmodel-Objekte initialisieren
-        _muzzleFlash        = new DynamicViewModelGameObject(viewModelMeshes.get("muzzleFlash"),        TurtleColor.CYAN,   new Vector3 (-2, -2.2,-11), new Vector3(), new Vector3(1.0, 1.0, 1.0));
-        _pistolMain         = new DynamicViewModelGameObject(viewModelMeshes.get("pistolMain"),         TurtleColor.GRAY,   new Vector3 (-1.5, -1,-10), new Vector3(), new Vector3(1.0, 1.0, 1.0));
-        _pistolDetails      = new DynamicViewModelGameObject(viewModelMeshes.get("pistolDetails"),      TurtleColor.ORANGE, new Vector3 (-1.5, -1,-10), new Vector3(), new Vector3(1.0, 1.0, 1.0));
-        _pistolHandsIdle    = new DynamicViewModelGameObject(viewModelMeshes.get("pistolHandsIdle"),    TurtleColor.WHITE,  new Vector3 (-1.5, -1,-10), new Vector3(), new Vector3(1.0, 1.0, 1.0));
-        _pistolHandsShot    = new DynamicViewModelGameObject(viewModelMeshes.get("pistolHandsShot"),    TurtleColor.WHITE,  new Vector3 (-1.5, -1,-10), new Vector3(), new Vector3(1.0, 1.0, 1.0));
-        _primaryMain        = new DynamicViewModelGameObject(viewModelMeshes.get("primaryMain"),        TurtleColor.GRAY,   new Vector3 (-2, -1,-11),   new Vector3(), new Vector3(1.0, 1.0, 1.0));
-        _primaryDetails     = new DynamicViewModelGameObject(viewModelMeshes.get("primaryDetails"),     TurtleColor.YELLOW, new Vector3 (-2, -1,-11),   new Vector3(), new Vector3(1.0, 1.0, 1.0));
-        _primaryHandsIdle   = new DynamicViewModelGameObject(viewModelMeshes.get("primaryHandsIdle"),   TurtleColor.WHITE,  new Vector3 (-2, -1,-11),   new Vector3(), new Vector3(1.0, 1.0, 1.0));
-        _primaryHandsShot   = new DynamicViewModelGameObject(viewModelMeshes.get("primaryHandsShot"),   TurtleColor.WHITE,  new Vector3 (-2, -1,-11),   new Vector3(), new Vector3(1.0, 1.0, 1.0));
-        _sniperMain         = new DynamicViewModelGameObject(viewModelMeshes.get("sniperMain"),         TurtleColor.GRAY,   new Vector3 (-1.5, 1,-12),  new Vector3(), new Vector3(1.0, 1.0, 1.0));
-        _sniperDetails      = new DynamicViewModelGameObject(viewModelMeshes.get("sniperDetails"),      TurtleColor.GREEN,  new Vector3 (-1.5, 1,-12),  new Vector3(), new Vector3(1.0, 1.0, 1.0));
+        _viewModel = new PlayerViewModel(viewModelMeshes, state, _mapHandler);
         
         _mapHandler.load("Level");
         
@@ -88,33 +73,7 @@ public class GameScene extends Scene
             Vector3 camDir = playerCam.getDirection();
             Vector3 camRight = playerCam.getRight();
             
-            // Schießen
-            if(_state.inputHandler.isKeyPressed(KeyCode.MOUSE_BUTTON_LEFT) && _timeSinceLastShot >= PISTOL_SHOT_COOLDOWN)
-            {
-                // Timer zurücksetzen
-                _timeSinceLastShot = 0.0;
-                
-                // TODO: Energie abziehen
-                
-                // Raycast vorbereiten
-                Vector2 source = new Vector2(_player.getPosition().getX(), _player.getPosition().getZ());
-                Vector2 dir = new Vector2(camDir.getX(), camDir.getZ()).invert();
-                double dist = 500.0;
-                ArrayList<RaycastHit> raycast = Physics.raycast(source, dir, dist, _mapHandler.getMap(), EnumSet.of(PhysicsLayer.SOLID, PhysicsLayer.ENEMY), EnumSet.of(PhysicsLayer.PLAYER));
-                
-                if(raycast.size() > 0 && raycast.get(raycast.size() - 1).collider.getLayer() == PhysicsLayer.ENEMY)
-                {
-                    ICollisionListener listener = raycast.get(raycast.size() - 1).collider.getListener();
-                    
-                    if(listener instanceof ILivingEntity)
-                    {
-                        ILivingEntity livingEntity = (ILivingEntity)listener;
-                        livingEntity.damage(12, "laser pistol shot");
-                    }
-                }
-                
-                _state.soundEngine.playSoundFromGroup("pistol_shot", 0.7, false);
-            }
+            
             
             // Tastenbelegungen
             if(_state.inputHandler.isKeyPressed(KeyCode.KEY_W))
@@ -140,11 +99,11 @@ public class GameScene extends Scene
      * @see Scene#update()
      */
     public void update(double deltaTime, double runTime)
-    {        
-        _timeSinceLastShot += deltaTime;
-        
+    {                
         // Updatet die Map
         _mapHandler.getMap().update(deltaTime, runTime, _player.getCamera().getPosition());
+        
+        _viewModel.update(deltaTime, runTime, _player.getCamera().getPosition());
         
         // Entfernt bereits durchgelaufene Sounds
         _state.soundEngine.removeStoppedSounds();
@@ -189,23 +148,13 @@ public class GameScene extends Scene
         // Cleart das Bild
         _state.renderer.clear(0, 0, 0);
         
-        // Draw Map
+        // Map zeichnen
         _mapHandler.getMap().draw(_state.renderer, playerCam);
         
-        // Draw Viewmodel
-        if(_state.inputHandler.isKeyPressed(KeyCode.MOUSE_BUTTON_LEFT))
-        {
-            _pistolHandsShot.draw(_state.renderer, playerCam);
-            _muzzleFlash.draw(_state.renderer, playerCam);
-        }
-        else
-        {
-            _pistolHandsIdle.draw(_state.renderer, playerCam);
-        }
-        _pistolMain.draw(_state.renderer, playerCam);
-        _pistolDetails.draw(_state.renderer, playerCam);
+        // Viewmodel (Hände, Waffe) zeichnen
+        _viewModel.draw(_state.renderer, playerCam);
         
-        // Draw UI
+        // User Interface zeichnen
         _state.renderer.drawCrosshair(8.0, 6.0, TurtleColor.GREEN);
         _state.renderer.drawHealthbar(_player);
         
